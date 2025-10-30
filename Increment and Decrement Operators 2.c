fcba4b5324131ef933a036b2d7be7fe4d5fde202
KATAKAM NAGA SAI YAMINI <nagasaiyaminik@gmail.com>
2025-10-30 11:58:40 +0530

    Add files via upload

---
+++ Increment and Decrement Operators 2.c
@@ -0,0 +1,12 @@
+// increment and decrement operators//
+#include<stdio.h>
+int main()
+{
+	int A=5;
+	A=A+2;
+	printf("%d",A++);
+	printf("%d",A);
+	printf("%d",--A);
+	printf("%d",A);
+	return 0;
+}
---
+++ ternary operators 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,max;
+	printf("enter 3 numbers");
+	scanf("%d %d %d",&a,&b,&c);
+	max=a>b? (a>c?a:c):(b>c?b:c);
+	printf("\n bignumber is : %d",max);
+	return 0;
+}
